9eb01dca78cad8d89389bdbe207f248eafcefa0a
a100535 <a100535@alunos.uminho.pt>
2022-11-16 11:34:27 +0000

    Add main

---
+++ trabalho-pratico/src/main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "userdata.h"
+#include "driverdata.h"
+#include "ridesData.h"
+#include "query_requests.h"
+#include "files.h"
+
+int main (int argc, char **argv) {
+    // se não for dado o ficheiro e as querries no terminal - fase 2
+    if (argc < 2) {
+        fprintf(stderr, "Modo interativo por implementar!\n");
+		exit(1);
+	}
+
+	//caso seja facultado o ficheiro e as querries no terminal - fase 1
+    FILE ** files = open_cmdfiles(argv); // array com pointers para os ficheiros {users,drivers,rides,querries}
+
+	DATA users = getUserData(files[0]);
+	
+	DATA drivers = getDriverData(files[1]);
+
+	DATA rides = getRidesData(files[2]);
+	
+	int ret = queryRequests(files[3], users, drivers, rides);
+    if (ret) {
+    	fprintf(stderr, "Error reading query requests");
+        return 2;
+    }
+
+	fclose(files[0]);
+	fclose(files[1]);
+	fclose(files[2]);
+	fclose(files[3]);
+
+	freeUserData(users);
+	freeDriverData(drivers);
+	freeRidesData(rides);
+	free(files);
+
+	return 0;
+}
