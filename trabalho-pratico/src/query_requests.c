57a16ea0ae8ea28218c9f21a7c109ed4ae7cbb41
Pedro Meruge <a100709@alunos.uminho.pt>
2023-01-25 01:21:57 +0000

    janela inicial e colunas no output das queries no modo interativo

--- trabalho-pratico/src/query_requests.c
+++ trabalho-pratico/src/query_requests.c
@@ -77,9 +77,43 @@ void printHelpCommands(GArray * splitQueryResults, int pageN, int numberOfPages,
     wrefresh(output);
 }
 
-// função que imprime os conteúdos de uma página, partindo os parâmetros de cada linha da string em diferentes colunas
-void printQueryOutput(GArray * splitQueryResults,int pageN, int * segmMaxSizes, int numberOfPages, int queryNumber, WINDOW * windows[]) {
-    char * queryDescription[TOTAL_QUERIES_NUMBER] = {"nome;genero;idade;avaliacao_media;numero_viagens;total_gasto",
+//escreve o output da query no ecrã, acrescentando espaços e linhas para organizar as palavras em colunas mais legíveis
+void printQueryStr(char * str, int * paramMaxSizes, WINDOW * window) {
+    int i, segmNumber, currentX = getcury(window), currentY;
+    while(str[0] != '\0') {
+        for (i=0, segmNumber=0, currentY = -1; str[i] != '\n' && str[i] != '\0'; i++) { 
+            if (str[i] == ';') {
+                str = str + i+1;
+                currentY += paramMaxSizes[segmNumber] + 1; // + 1, simula o incremento que é feito no pointer digital, quando se usa addch;
+                wmove(window,currentX,currentY);
+                i=-1;
+                segmNumber++;
+                waddch(window,'|');
+            }
+            else wprintw(window,"%c",str[i]); // printw permite UTF-8, enquanto addch não
+       }
+        str = str + i + 1;
+        currentX++; // desce uma linha o cursor
+        if (str[-1] == '\0') str--; // no caso de paragem, a string aponta para o '\0'
+        waddch(window,'\n');
+     }
+    
+    //tentativa anterior
+    // for(i=0, segmNumber = 0; str[i] != '\0'; segmNumber++, i++) { 
+    //     str = str + i;
+    //     for (i = 0, currentParamMax = paramMaxSizes[segmNumber]; str[i] != ';' && str[i] != '\0'; i++); // detetar o fim de um parâmetro, ou o fim de uma linha
+    //     str[i] = '\0'; // substitui o ';' ou '\n' por '\0' para poder imprimir esse segmento de string apenas // não fuciona para strings alocadas na staack como as queryDescriptions!!
+    //     waddstr(window,str);
+    //     for(temp = i; temp < currentParamMax; waddch(window,' '),temp ++); // adicionar espaços até chegar ao fim da coluna
+    //     waddch(window,'|');
+    // }
+    // wdelch(window); wdelch(window);
+}
+
+// função que imprime os conteúdos de uma página de output de uma query, partindo os parâmetros de cada linha da string em diferentes colunas
+void printQueryOutput(GArray * splitQueryResults,int pageN, int * paramMaxSizes, int numberOfPages, int queryNumber, WINDOW * windows[]) {
+    // o print das queries inclui uma descrição do que é cada parâmetro de output
+    char * queryDescriptions[TOTAL_QUERIES_NUMBER] = {"nome;genero;idade;avaliacao_media;numero_viagens;total_gasto",
                                                      "id;nome;avaliacao_media",
                                                      "username;nome;distancia_total",
                                                      "preco_medio",
@@ -87,22 +121,51 @@ void printQueryOutput(GArray * splitQueryResults,int pageN, int * segmMaxSizes, 
                                                      "distancia_media",
                                                      "id;nome;avaliacao_media",
                                                      "id_condutor;nome_condutor;username_utilizador;nome_utilizador",
-                                                     "id_viagem;data_viagem;distancia;cidade;valor_gorjeta"};
+                                                     "id_viagem;data_viagem;distancia;cidade;valor_gorjeta"}; 
+                                                     
+    int i, segmNumber, totalChar;
     WINDOW * output = windows[2];
-    char * currPage = g_array_index(splitQueryResults,char *, pageN-1);  // string da página atual (página pageN)
-    wclear(output);
-    wprintw(output,"# Query %d output\nFormato: %s\n---\n",queryNumber,queryDescription[queryNumber-1]);
+    char * currPage = g_array_index(splitQueryResults,char *, pageN-1),  // string da página atual (página pageN)
+         * currentDescription = queryDescriptions[queryNumber-1];
 
-    wprintw(output,"%s\n",currPage);
+    // calcular se alguma descrição ocupa mais caractéres que o max de caractéres do respetivo parâmetro do output
+    for(i=0,segmNumber = 0, totalChar = 0; currentDescription[i] != '\0'; segmNumber++, i++) { 
+        currentDescription = currentDescription + i;
+        for (i = 0; currentDescription[i] != ';' && currentDescription[i] != '\0'; i++);
+        if (i > paramMaxSizes[segmNumber]) paramMaxSizes[segmNumber] = i;
+        totalChar += paramMaxSizes[segmNumber];
+        if (currentDescription[i] == '\0') i--;
+    }
+
+    //calcular se existe espaço no terminal para expandir o tamanho de cada coluna, para a tabela ficar proporcional ao tamanho do terminal
+    int spareChar = getmaxx(output) - totalChar - segmNumber + 2; // no fim do loop anterior, segmNumber guarda o nº de parâmetros na query atual, totalChar guarda o nº de char numa linha, ocupado pelo output
+    spareChar /= segmNumber; // spareChar representa o nº de caractéres adicional que se pode acrescentar a cada tabela
+    
+    wclear(output);
+    for(i = 0; i < segmNumber; i++) {
+        wprintw(output,"%d;", paramMaxSizes[i]);
+    }
+    waddch(output,'\n');
+    if (spareChar > 0) for(i = 0; i < segmNumber; paramMaxSizes[i] += spareChar, i++);
+
+    for(i = 0; i < segmNumber; i++) {
+        wprintw(output,"%d;", paramMaxSizes[i]);
+    }
+
+    wprintw(output,"segmNumber:%d, spareChar: %d, # Query %d output\n---\n",segmNumber, spareChar, queryNumber);
+
+    printQueryStr(queryDescriptions[queryNumber-1],paramMaxSizes, output); // print da descrição da query
+    printQueryStr(currPage,paramMaxSizes,output); // print do output da query
+    //wprintw(output,"%s\n",currPage);
 
     mvwprintw(output,getmaxy(output)-2,0,"---\nPage %d-%d",pageN, numberOfPages);
     wrefresh(output);
 }
 
 // parte a string em conjuntos de n linhas, colocando-as no GArray
-// a sepração de strings garante que informações sobre uma query específica não são separados em páginas diferentes
+// a sepração de strings garante que informações sobre uma query específica não são separados em páginas diferentes (para definir uma secção que não deve ser separada em páginas diferentes, ela tem de acabar com 2 '\n' seguidos)
 GArray * splitString (char * str, int numberOfLines, int numberOfColumns) {
-    int i, counter, lineTracker,groupTracker; // lineTracker contabiliza linhas extras, quando a frase não cabe numa linha do terminal, groupTracker garante que não aparece partes de informação de uma query em páginas diferentes
+    int i, counter, lineTracker, groupTracker; // lineTracker contabiliza linhas extras, quando a frase não cabe numa linha do terminal, groupTracker garante que não aparece partes de informação de uma query em páginas diferentes
     GArray * splitStrResults = g_array_new(FALSE, FALSE, sizeof(char *));
     while (str[0] != '\0') {
         for(i = 0, lineTracker = 0, groupTracker = 0, counter = 0; str[i] != '\0' && counter < numberOfLines; lineTracker++,i++) {
@@ -116,7 +179,7 @@ GArray * splitString (char * str, int numberOfLines, int numberOfColumns) {
 
         }
         str[i-1] = '\0';
-        if(str[i] == '\n') i++; // nos casos em que para na 1temos duas newlines seguidas (fim da descrição de uma querry), salta-se a segunda para não passar para a próxima página e desformatar
+        if(str[i] == '\n') i++; // nos casos em que temos duas newlines seguidas (fim da descrição de uma querry), parando na primeira, salta-se a segunda para não passar para a próxima página e desformatar
         g_array_append_val(splitStrResults,str);
         str = str + i;
         }
@@ -128,10 +191,9 @@ GArray * splitString (char * str, int numberOfLines, int numberOfColumns) {
 //supõe-se que cada linha de input (linhas acabam em \n) cabe numa linha do terminal
 // NOTA: se recebesse o nº de linhas da str, originalmente, a conta ficava mais simples, evita-se o GArray! Bastava calcular nº de '\n' na str original e somar 1 para a última linha -> não basta receber o nº de linhas, porque pode variar se uma linha inteira não couber numa janela!
 
-GArray * splitStringMaxParam(char * queryResult, int numberOfLines, int * paramMaxSizes) {
-    int i, newlineCounter, segmNumber, segmMaxSize;
-
-    paramMaxSizes = calloc(MAX_QUERY_OUTPUTS,sizeof(int)); // array que guarda o tamanho da maior palavra, para cada segmento do output
+GArray * splitStringMaxParam(char * queryResult, int numberOfLines, int * * arrayToFill) {
+    int i, newlineCounter, segmNumber, segmMaxSize,
+        * paramMaxSizes = calloc(MAX_QUERY_OUTPUTS,sizeof(int)); // array que guarda o tamanho da maior palavra, para cada segmento do output
     GArray * splitQueryResults = g_array_new(FALSE, FALSE, sizeof(char *)); // gArray para realocar o array automaticamente
 
     while (queryResult[0] != '\0') {
@@ -146,6 +208,7 @@ GArray * splitStringMaxParam(char * queryResult, int numberOfLines, int * paramM
         queryResult = queryResult + i-1;
     }
     paramMaxSizes[0] ++; // devido à forma como o loop foi feito (para n ter condições if a mais) inicia-se i=1, daí ter de se incrementar em 1 o valor segmMaxSizes[0]
+    (*arrayToFill) = paramMaxSizes;
     return splitQueryResults;
 }
 
@@ -162,7 +225,7 @@ void printResultInPages(char * queryResult, int queryNumber, WINDOW * windows[])
 
     //separação da string em segmentos, um para cada página; cálculo dos caractéres necessários para cada parâmetro (apenas quando escreve output de uma query)
     int * segmMaxSizes = NULL;
-    GArray * splitQueryResults = (queryNumber != -1) ? splitStringMaxParam(queryResult,getmaxy(output)-5, segmMaxSizes) 
+    GArray * splitQueryResults = (queryNumber != -1) ? splitStringMaxParam(queryResult,getmaxy(output)-5, &segmMaxSizes) 
                                                      : splitString(queryResult,getmaxy(output)-4,getmaxx(output));
 
     //apresentação da primeira página (acontece sempre)
@@ -203,6 +266,22 @@ void printResultInPages(char * queryResult, int queryNumber, WINDOW * windows[])
     g_array_free(splitQueryResults,TRUE); // dá free das strings, ou não? (isso já é feito na interactRequests)
 }
 
+// print da tela inicial
+void printBeginScreen(WINDOW * beginWindow, int maxX, int maxY) {
+    char * initMsg[] = {"<Modo interativo>","Pressione qualquer tecla para iniciar"};
+    curs_set(0); // meter o cursor invisível na tela inicial
+    attron(A_BOLD);
+    mvaddstr(maxX/2, (maxY - strlen(initMsg[0]))/2, initMsg[0]);
+    attron(A_BLINK);
+    mvaddstr(maxX/2 + 1, (maxY - strlen(initMsg[1]))/2, initMsg[1]);
+    attroff(A_BOLD | A_BLINK);
+    wborder(stdscr,'#','#','#','#','#','#','#','#');
+    refresh();
+    getch(); // para a tela até receber qualquer input
+    curs_set(1); // meter o cursor visível de novo
+    clear();
+}
+
 //ações para quando o user escreve "help", no modo interativo
 void helpCommands (WINDOW * windows[]) {
     int i;
@@ -232,23 +311,12 @@ void helpCommands (WINDOW * windows[]) {
     char helpInstructions[TOTAL_QUERIES_NUMBER * 250] = "", buffer[250]; // 250 é o número de caractéres que a explicação de cada query ocupa aproximadamente
 
     for (i = 0; i < TOTAL_QUERIES_NUMBER; i++) {
-        sprintf(buffer,"Q%d| Formato: \"%s\"\n    Descrição: %s\n    Exemplo: \"%s\"\n\n", i+1, queryFormats[i],queryDescriptions[i],queryExamples[i]);
+        sprintf(buffer,"Q%d|\nFormato: \"%s\"\nDescrição: %s\nExemplo: \"%s\"\n\n", i+1, queryFormats[i],queryDescriptions[i],queryExamples[i]);
         strcat(helpInstructions,buffer);
     }
     //waddstr(output,helpInstructions);
-    printResultInPages(helpInstructions,-1,windows);
+    printResultInPages(helpInstructions,-1,windows); // apresentação do output em páginas
 
-// pode ser usado para fazer secção das páginas girar automaticamente !!!
-    //int terminalLines = 0, maxScroll = getmaxy(output) -1;
-    // scrollok(output,TRUE);
-    // wsetscrreg(output,terminalLines,maxScroll);
-    // wrefresh(output);
-
-    // for (i = 1; i <= 5; i++){
-    //     napms(500);
-    //wscroll(output);
-    //wrefresh(output);
-    // }
     wrefresh(output);
 }
 
@@ -256,33 +324,23 @@ void helpCommands (WINDOW * windows[]) {
 // TODO: Error check de inputs para queries no modo interativo
 int interactRequests(UserData *userData, DriverData *driverData, RidesData *ridesData) {
     setlocale(LC_ALL, "");
+    setlocale(LC_NUMERIC,"C");
     initscr(); //iniciar o ncurses
-    // if (curs_set(0) == ERR) {
-    //     fprintf(stderr,"terminal doesn´t suport invisible cursor");
-    //     exit(-1);
-    // }
+
     int maxX,maxY;
     getmaxyx(stdscr,maxX, maxY); // receber o nº de de row and columns
     if (maxX< 24 || maxY < 80) {
         fprintf(stderr,"Screen size is too small! Run the program again with standard terminal size (80,24) or bigger\n");
         return 1;
     }
-    //char * initMsg[] = {"<Modo interativo>","Pressione qualquer tecla para iniciar"};
-    //apresentação da tela inicial
-    // attron(A_BOLD);
-    // mvaddstr(maxX/2, (maxY - strlen(initMsg[0]))/2, initMsg[0]);
-    // attron(A_BLINK);
-    // mvaddstr(maxX/2 + 1, (maxY - strlen(initMsg[1]))/2, initMsg[1]);
-    // attroff(A_BOLD | A_BLINK);
-    // wborder(stdscr,'#','#','#','#','#','#','#','#');
-    // refresh();
-    // getch(); // para a tela até receber qualquer input
-    // clear();
+
+    //janela inicial; ao pressionar uma tecla salta para a página de input nas queries
+    printBeginScreen(stdscr,maxX,maxY);
 
     //janela das queries
-    WINDOW * tabs = newwin(3,maxY,0,0), //janela "tabs", height = 3, width = maxX, posInicial(0,0);
+    WINDOW * tabs = newwin(3,maxY,0,0), 
     * outputBorder = newwin(maxX-6,maxY,3,0), // borda da janela "output" (assim não é afetada por newlines escritas na subjanela)
-    * output = newwin(maxX-8,maxY-2,4,1), // janela "outputs"
+    * output = newwin(maxX-8,maxY-2,4,1),
     * inputBorder = newwin(3,maxY,maxX-3,0),
     * input = newwin(1,maxY-2,maxX-2,1),
     * allWindows[] = {tabs,outputBorder,output,inputBorder,input};
@@ -306,10 +364,10 @@ int interactRequests(UserData *userData, DriverData *driverData, RidesData *ride
     int commandN = 1; // só para debug, pode-se remover depois
 
     while (wgetnstr(input,strBuffer, INPUT_STR_BUFF_SIZE) == OK) { // receber input no terminal
-        if (!strcmp(strBuffer,"exit")) {
-            return 0;
+        if (!strcmp(strBuffer,"exit")) { // se quiser sair do programa
+            break;
         }
-        else if (!strcmp(strBuffer,"help")) {
+        else if (!strcmp(strBuffer,"help")) { // se quiser os comandos de ajuda
             helpCommands(allWindows);
         }
         else if (validQueryInput(strBuffer)) { // se o input for válido para uma query, calcula-se a resposta
@@ -328,6 +386,7 @@ int interactRequests(UserData *userData, DriverData *driverData, RidesData *ride
         }
         else {
             waddstr(output,"(!) Input inválido\n\nEscreva \"help\" na consola para saber todos os comandos disponíveis e o seus formatos\n\n");
+            wrefresh(output);
         }
         // recriar linha de input com apenas a string "Input:"
         refreshWindow(input,"Input:");
